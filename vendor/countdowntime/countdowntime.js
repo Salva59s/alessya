40ecf7eafbdae2499ab45c4ea61a544625f20a73
Salva59s <48624308+Salva59s@users.noreply.github.com>
2019-08-27 17:33:34 +0200

    1

--- vendor/countdowntime/countdowntime.js
+++ vendor/countdowntime/countdowntime.js
@@ -39,48 +39,12 @@
             var deadline = new Date(Date.parse(new Date()) + endDate * 24 * 60 * 60 * 1000 + endHours * 60 * 60 * 1000); 
           }
           
-          
-          initializeClock(deadline);
-
-          function getTimeRemaining(endtime) { 
-            var t = Date.parse(endtime) - Date.parse(new Date());
-            var seconds = Math.floor((t / 1000) % 60);
-            var minutes = Math.floor((t / 1000 / 60) % 60);
-            var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-            var days = Math.floor(t / (1000 * 60 * 60 * 24));
-            return {
-              'total': t,
-              'days': days,
-              'hours': hours,
-              'minutes': minutes,
-              'seconds': seconds
-            };
-          }
-
-          function initializeClock(endtime) { 
-            var daysSpan = $(obj).find('.days');
-            var hoursSpan = $(obj).find('.hours');
-            var minutesSpan = $(obj).find('.minutes');
-            var secondsSpan = $(obj).find('.seconds');
-
-            function updateClock() { 
-              var t = getTimeRemaining(endtime);
-
-              daysSpan.html(t.days);
-              hoursSpan.html(('0' + t.hours).slice(-2));
-              minutesSpan.html(('0' + t.minutes).slice(-2));
-              secondsSpan.html(('0' + t.seconds).slice(-2))
-
-              if (t.total <= 0) {
-                clearInterval(timeinterval);
-              }
-            }
-
-            updateClock();
-            var timeinterval = setInterval(updateClock, 1000);
-          }
-
-          
+          var t = Date.parse(deadline) - Date.parse(new Date());
+            
+          var clock = $(obj).FlipClock(t/1000, {
+            clockFace: 'DailyCounter',
+            countdown: true
+          });
 
 
         });
